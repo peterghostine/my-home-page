744e584ad82766d697151d33638d8f2cd5ad4211
Peter Ghostine <peterghostine@gmail.com>
2021-09-18 17:33:27 -0400

    Fixed menu behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 // Menu behavior
 
 $(document).ready(function() {
-  $(".menu-toggle").click(function() {
-    $("nav").toggleClass("showing"); 
-  });
 
-  $("nav > ul").mouseleave(function() {
-    if ($(".menu-toggle").is(":visible")) {
-      $("nav").toggleClass("showing"); 
+/*
+  $(".menu-toggle").click(function() {
+    $("nav > ul > li > ul").slideUp();
+    $("nav > ul").toggleClass("showing"); 
+  });
+*/
+
+  $(document).on('click', function(event) {
+    if ($("nav > ul").is(":visible")) {
+      if (!$(event.target).closest("nav > ul").length) {
+        $("nav > ul > li > ul").slideUp();
+        $("nav > ul").toggleClass("showing");
+      };  
+    } else if (($(event.target).closest(".menu-toggle").length)) {
+        $("nav > ul").toggleClass("showing"); 
     };
   });
-   
+
   $("nav > ul > li").click(function() { 
     $("nav > ul > li:hover > ul").slideToggle();
   });
+  
 
   $("nav > ul > li").mouseleave(function() {
-    $("nav > ul > li > ul").slideUp(); 
+    if ($(".menu-toggle").is(":hidden")) {
+      $("nav > ul > li > ul").slideUp(); 
+    };
   });   
 });
 
