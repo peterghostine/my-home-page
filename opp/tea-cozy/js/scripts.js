86e013feeb3dc219e50553afe075913749aab65d
Peter Ghostine <peterghostine@gmail.com>
2021-09-19 23:07:41 -0400

    completed Tea Cozy project

---
+++ opp/tea-cozy/js/scripts.js
@@ -0,0 +1,39 @@
+// Menu behavior
+
+/*
+$(document).ready(function() {  
+    $(document).on('click', function(event) {
+      if ($("nav > ul").is(":visible")) {
+        if (!$(event.target).closest("nav > ul").length) {
+          $("header > nav > ul").toggleClass("showing");
+        };  
+      } else if (($(event.target).closest(".menu-toggle").length)) {
+          $("nav > ul").toggleClass("showing"); 
+      };
+    });
+  
+    $("nav > ul > li").click(function() { 
+      $("nav > ul > li:hover > ul").slideToggle();
+    });
+    
+  
+    $("nav > ul > li").mouseleave(function() {
+      if ($(".menu-toggle").is(":hidden")) {
+        $("nav > ul > li > ul").slideUp(); 
+    };
+  });   
+});
+*/
+
+$(document).ready(function() {  
+  $(document).on('click', function(event) {
+    if ($("header nav").is(":visible")) {
+      if (!$(event.target).closest("header nav").length) {
+        $("header nav").toggleClass("showing");
+      };  
+    } else if (($(event.target).closest(".menu-toggle").length)) {
+        $("header > nav").toggleClass("showing"); 
+    };
+  });
+});
+   
