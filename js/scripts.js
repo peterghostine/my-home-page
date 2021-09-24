b0b3bec1e1a256d51abd1956201efed4098d3d29
Peter Ghostine <peterghostine@gmail.com>
2021-09-24 14:46:09 -0400

    Made minor changes and started adding content.

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
 $('.post-wrapper').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
-  autoplay: false,
+  autoplay: true,
   autoplaySpeed: 2000,
   nextArrow: $('.next'),
   prevArrow: $('.prev'),
