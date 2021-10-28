9cd0c3f819488c5215754f57d8d95363d9aaf32e
Peter Ghostine <peterghostine@gmail.com>
2021-10-28 12:56:34 -0400

    Added Shop and Photos.

--- opp/tpt/scripts/menu.js
+++ opp/tpt/scripts/menu.js
@@ -1,28 +1,30 @@
 // Menu behavior
 
-$(document).ready(function() {  
-    $(document).on('click', function(event) {
-      if ($("nav > ul").is(":visible")) {
-        if (!$(event.target).closest("nav > ul").length) {
-          $("header > nav > ul").toggleClass("showing");
-        };  
-      } else if (($(event.target).closest(".menu-toggle").length)) {
-          $("nav > ul").toggleClass("showing"); 
-      };
-    });
-  
-    $("nav > ul > li").click(function() { 
-      $("nav > ul > li:hover > ul").slideToggle();
-    });
-    
-  
-    $("nav > ul > li").mouseleave(function() {
-      if ($(".menu-toggle").is(":hidden")) {
-        $("nav > ul > li > ul").slideUp(); 
-    };
-  });   
+$(document).ready(function() {
+	$(document).on('click', function(event) {
+		if ($("nav > ul").is(":visible")) {
+			if (!$(event.target).closest("nav > ul").length) {
+				$("header > nav > ul").toggleClass("showing");
+			};
+		} else if (($(event.target).closest(".menu-toggle").length)) {
+			$("nav > ul").toggleClass("showing");
+		};
+	});
+
+	$("nav > ul > li").click(function() {
+		$("nav > ul > li:hover > ul").slideToggle();
+	});
+
+
+	$("nav > ul > li").mouseleave(function() {
+		if ($(".menu-toggle").is(":hidden")) {
+			$("nav > ul > li > ul").slideUp();
+		};
+	});
 });
 
 $(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
+	$('.image-link').magnificPopup({
+		type: 'image'
+	});
 });
--- opp/tpt/scripts/modal-img.js
+++ opp/tpt/scripts/modal-img.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {  
-    const Images = document.getElementsByClassName("image");
+$(document).ready(function() {
+	const Images = document.getElementsByClassName("image");
 
-const modal = document.getElementById("modal-container");
-let modalImg = document.getElementById("modal-img");
+	const modal = document.getElementById("modal-container");
+	let modalImg = document.getElementById("modal-img");
 
-Array.prototype.forEach.call(Images, (item) => {
-    item.addEventListener('click', () => {
-        modalImg.src = item.src;
-        modal.style.display = "flex";
-        modal.style.alignItems = "center";
-    });    
-});
+	Array.prototype.forEach.call(Images, (item) => {
+		item.addEventListener('click', () => {
+			modalImg.src = item.src;
+			modal.style.display = "flex";
+			modal.style.alignItems = "center";
+		});
+	});
 
-// Get the <span> element that closes the modal.
-var close = document.getElementById("close");
+	// Get the <span> element that closes the modal.
+	var close = document.getElementById("close");
 
-// When the user clicks on <span> (x), close the modal.
-close.onclick = function() { 
-  modal.style.display = "none";
-}
+	// When the user clicks on <span> (x), close the modal.
+	close.onclick = function() {
+		modal.style.display = "none";
+	}
 
 });
